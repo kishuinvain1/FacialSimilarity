c2266fb0812bb850534e38fecb1930dd384e5079
kishuinvain1 <37648429+kishuinvain1@users.noreply.github.com>
2023-04-09 15:24:01 +0530

    Update streamlit_app_predict.py

--- streamlit_app_predict.py
+++ streamlit_app_predict.py
@@ -12,7 +12,7 @@ import base64
 
 
 
-def load_image1():
+def load_image():
     opencv_image = None 
     path = None
     f = None
@@ -33,25 +33,7 @@ def load_image1():
     return path, opencv_image
        
 
-def load_image2():
-    opencv_image = None 
-    path = None
-    f = None
-    uploaded_file = st.file_uploader(label='Pick an image to test')
-    print(uploaded_file)
-    if uploaded_file is not None:
-        file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-        opencv_image = cv2.imdecode(file_bytes, 1)
-        image_data = uploaded_file.getvalue() 
-        #st.image(image_data)
-        name = uploaded_file.name
-        path = os.path.abspath(name)
-        print("abs path")
-        print(path)
-	
-        cv2.imwrite("main_image.jpg", opencv_image)
-       
-    return path, opencv_image
+
        	
 	
 
@@ -62,13 +44,15 @@ def load_image2():
 def main():
     st.title('Face Similiarity Check')	
    
-    image1, svd_img1 = load_image1()
-    image2, svd_img2 = load_image2()
+    image1, svd_img1 = load_image()
+    image2, svd_img2 = load_image()
    
     result = st.button('Predict')
     if(result):
         st.image(svd_img1, caption="image1")
         st.image(svd_img2, caption="image2")
+	#extractFace(svd_img1)
+	#extractFace(svd_img2)
         
 
 

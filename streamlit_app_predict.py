f536b19093a7e1889cd37f7580c91528e8afe961
kishuinvain1 <37648429+kishuinvain1@users.noreply.github.com>
2023-04-09 15:33:06 +0530

    Update streamlit_app_predict.py

--- streamlit_app_predict.py
+++ streamlit_app_predict.py
@@ -12,29 +12,27 @@ import base64
 
 
 
-def load_image():
+def load_image1():
     opencv_image = None 
     path = None
     f = None
-    uploaded_file = st.file_uploader(label='Pick an image to test')
-    print(uploaded_file)
-    if uploaded_file is not None:
-        file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-        opencv_image = cv2.imdecode(file_bytes, 1)
-        image_data = uploaded_file.getvalue() 
-        #st.image(image_data)
-        name = uploaded_file.name
-        path = os.path.abspath(name)
-        print("abs path")
-        print(path)
-	
-        cv2.imwrite("main_image.jpg", opencv_image)
-       
-    return path, opencv_image
-       
+    img_list = []
+    uploaded_files = st.file_uploader(label='Upload images to find the similarity', accept_multiple_files=True)
+    for uploaded_file in uploaded_files: 
+        if uploaded_file is not None:
+            file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+            opencv_image = cv2.imdecode(file_bytes, 1)
+            image_data = uploaded_file.getvalue() 
+            #st.image(image_data)
+            name = uploaded_file.name
+            path = os.path.abspath(name)
+            print("abs path")
+            print(path)
+            img_list.append(opencv_image)
+    return img_list
 
 
-       	
+
 	
 
 
@@ -44,15 +42,16 @@ def load_image():
 def main():
     st.title('Face Similiarity Check')	
    
-    image1, svd_img1 = load_image()
-    image2, svd_img2 = load_image()
+    svd_img_list = load_image()
+    
    
     result = st.button('Predict')
     if(result):
-        st.image(svd_img1, caption="image1")
-        st.image(svd_img2, caption="image2")
-	#extractFace(svd_img1)
-	#extractFace(svd_img2)
+        for image in svd_img_list:
+            st.image(svd_img1, caption="image1")
+       
+	    #extractFace(svd_img1)
+
         
 
 
